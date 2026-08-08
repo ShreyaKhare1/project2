a5e40d54b67d3072f2221b66ec7350b88143fcbe
ShreyaKhare1 <120880000+ShreyaKhare1@users.noreply.github.com>
2026-08-08 09:40:31 +0530

    Update OpenAccount.js

--- frontend/src/OpenAccount.js
+++ frontend/src/OpenAccount.js
@@ -5,7 +5,7 @@ function OpenAccount() {
   return ( 
     <div className='container'>
       <div className='row text-center'>
-        <h2 className='mb-4'> Open a Zerodha account</h2>
+        <h2 className='mb-4'> Open a account</h2>
         <p className='text-muted mt-2 mb-4'>Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
         <button className='btn btn-primary m-auto mt-2' style={{width:"20%"}}  onClick={() => navigate("/signup")}>Sign up for free</button>
       </div>
