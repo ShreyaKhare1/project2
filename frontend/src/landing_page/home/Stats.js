0e218fb08ff5204a578c31e731d6242d94e3641b
ShreyaKhare1 <120880000+ShreyaKhare1@users.noreply.github.com>
2026-08-08 09:41:30 +0530

    Update Stats.js

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -6,12 +6,12 @@ function Stats() {
         <div className='col-6 p-5'>
           <h3 className='mb-5'>Trust with confidence</h3>
           <h4 className='mt-5'>Customer-first always</h4>
-          <p className='text-muted'>That's why 1.6+ crore customers trust Zerodha with ~ ₹6 lakh crores of equity investments, making us India’s largest broker; contributing to 15% of daily retail exchange volumes in India.</p>
+          <p className='text-muted'>That's why 1.6+ crore customers trust us with ~ ₹6 lakh crores of equity investments, making us India’s largest broker; contributing to 15% of daily retail exchange volumes in India.</p>
           
           <h4 className='mt-5'>No spam or gimmicks</h4>
           <p className='text-muted'>No gimmicks, spam, "gamification", or annoying push notifications. High quality apps that you use at your pace, the way you like. Our philosophies.</p>
           
-          <h4 className='mt-5'>The Zerodha universe</h4>
+          <h4 className='mt-5'>The universe</h4>
           <p className='text-muted'>Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
          
           <h4 className='mt-5'>Do better with money</h4>
