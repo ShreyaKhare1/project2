6d24368700c9c4703de58a34275753b65aa9cf47
ShreyaKhare1 <120880000+ShreyaKhare1@users.noreply.github.com>
2026-09-01 20:13:29 +0530

    Delete backend/model/SentimentModel.js

--- backend/model/SentimentModel.js
+++
@@ -1,4 +0,0 @@
-const{model}=require("mongoose");
-const {SentimentSchema}=require("../schemas/SentimentSchema");
-const SentimentModel=new model("sentiment",SentimentSchema);
-module.exports={SentimentModel};
